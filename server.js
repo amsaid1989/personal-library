33fbe708a6c419aee2ea52036537d3edafc7cde1
Abdelrahman <said.abdelrahman89@gmail.com>
2021-05-28 02:25:19 +0100

    Testing a different approach

--- server.js
+++ server.js
@@ -16,40 +16,52 @@ app.use("/public", express.static(process.cwd() + "/public"));
 
 app.use(cors({ origin: "*" })); //USED FOR FCC TESTING PURPOSES ONLY!
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+mongoose
+    .connect(process.env.DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    })
+    .then(() => {
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({ extended: true }));
 
-//Index page (static HTML)
-app.route("/").get(function (req, res) {
-    res.sendFile(process.cwd() + "/views/index.html");
-});
+        //Index page (static HTML)
+        app.route("/").get(function (req, res) {
+            res.sendFile(process.cwd() + "/views/index.html");
+        });
 
-//For FCC testing purposes
-fccTestingRoutes(app);
+        //For FCC testing purposes
+        fccTestingRoutes(app);
 
-//Routing for API
-apiRoutes(app);
+        //Routing for API
+        apiRoutes(app);
 
-//404 Not Found Middleware
-app.use(function (req, res, next) {
-    res.status(404).type("text").send("Not Found");
-});
+        //404 Not Found Middleware
+        app.use(function (req, res, next) {
+            res.status(404).type("text").send("Not Found");
+        });
+
+        if (process.env.NODE_ENV === "test") {
+            console.log("Running Tests...");
+            setTimeout(function () {
+                try {
+                    runner.run();
+                } catch (e) {
+                    let error = e;
+                    console.log("Tests are not valid:");
+                    console.log(error);
+                }
+            }, 1500);
+        }
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 
 //Start our server and tests!
 app.listen(process.env.PORT || 3000, function () {
     console.log("Listening on port " + process.env.PORT);
-    if (process.env.NODE_ENV === "test") {
-        console.log("Running Tests...");
-        setTimeout(function () {
-            try {
-                runner.run();
-            } catch (e) {
-                let error = e;
-                console.log("Tests are not valid:");
-                console.log(error);
-            }
-        }, 1500);
-    }
 });
 
 module.exports = app; //for unit/functional testing
