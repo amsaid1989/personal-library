a13493d82bbb9ca99c042d83ca441d1fc61e9f94
Abdelrahman <said.abdelrahman89@gmail.com>
2021-05-28 01:57:34 +0100

    Connecting to the database happens in the app.listen function

--- server.js
+++ server.js
@@ -16,52 +16,51 @@ app.use("/public", express.static(process.cwd() + "/public"));
 
 app.use(cors({ origin: "*" })); //USED FOR FCC TESTING PURPOSES ONLY!
 
-mongoose
-    .connect(process.env.DB, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-    })
-    .then(() => {
-        app.use(bodyParser.json());
-        app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-        //Index page (static HTML)
-        app.route("/").get(function (req, res) {
-            res.sendFile(process.cwd() + "/views/index.html");
-        });
+//Index page (static HTML)
+app.route("/").get(function (req, res) {
+    res.sendFile(process.cwd() + "/views/index.html");
+});
 
-        //For FCC testing purposes
-        fccTestingRoutes(app);
+//For FCC testing purposes
+fccTestingRoutes(app);
 
-        //Routing for API
-        apiRoutes(app);
+//Routing for API
+apiRoutes(app);
 
-        //404 Not Found Middleware
-        app.use(function (req, res, next) {
-            res.status(404).type("text").send("Not Found");
-        });
-
-        if (process.env.NODE_ENV === "test") {
-            console.log("Running Tests...");
-            setTimeout(function () {
-                try {
-                    runner.run();
-                } catch (e) {
-                    let error = e;
-                    console.log("Tests are not valid:");
-                    console.log(error);
-                }
-            }, 1500);
-        }
-    })
-    .catch((err) => {
-        console.error(err);
-    });
+//404 Not Found Middleware
+app.use(function (req, res, next) {
+    res.status(404).type("text").send("Not Found");
+});
 
 //Start our server and tests!
 app.listen(process.env.PORT || 3000, function () {
     console.log("Listening on port " + process.env.PORT);
+    mongoose
+        .connect(process.env.DB, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        })
+        .then(() => {
+            if (process.env.NODE_ENV === "test") {
+                console.log("Running Tests...");
+                setTimeout(function () {
+                    try {
+                        runner.run();
+                    } catch (e) {
+                        let error = e;
+                        console.log("Tests are not valid:");
+                        console.log(error);
+                    }
+                }, 1500);
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+        });
 });
 
 module.exports = app; //for unit/functional testing
